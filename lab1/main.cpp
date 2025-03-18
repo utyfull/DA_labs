d52f6b00f3ac6d36ebe2cd31556592848a28279d
Utyfull <serg_misha1@mail.ru>
2025-03-18 19:03:57 +0000

    1-st lab ok!

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,57 +1,61 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
+#include <string_view>
 
-void qsortRecursive(std::vector<std::string>& mas, int size, size_t index) {
-    int i = 0;
-    int j = size - 1;
+void countingSortByChar(const std::vector<std::string_view>& arr,
+                        std::vector<std::string_view>& output, int pos) {
+    int n = arr.size();
+    int count[128] = {0};
+    output.resize(n);
 
-    char mid = mas[size / 2][index];
-
-    do {
-        while (mas[i][index] < mid) {
-            i++;
-        }
-
-        while (mas[j][index] > mid) {
-            j--;
-        }
-
-        if (i <= j) {
-            std::swap(mas[i], mas[j]);
-            i++;
-            j--;
-        }
-    } while (i <= j);
-
-    if (j > 0) {
-        qsortRecursive(mas, j + 1, index);
+    for (int i = 0; i < n; i++) {
+        char ch = arr[i][pos];
+        count[(int)ch]++;
     }
-    if (i < size) {
-        qsortRecursive(mas, size - i, index);
+    for (int i = 1; i < 128; i++) {
+        count[i] += count[i - 1];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        char ch = arr[i][pos];
+        output[--count[(int)ch]] = arr[i];
     }
 }
 
 int main() {
-
-    std::string in;
-
-    std::vector<std::string> data;
-
-    while (getline(std::cin, in)) {
-        data.push_back(in);
-    }
-
-    for (size_t i = 0; i < 8; i++) {
-
-        if (i != 1 && i != 5) {
-
-            qsortRecursive(data, data.size(), i);
-
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    std::string buffer;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        if (!line.empty()) {
+            buffer += line;
+            buffer.push_back('\n');
         }
     }
     
-    for (size_t i = 0; i < data.size(); i++) {
-        std::cout << data[i] << std::endl;
+    std::vector<std::string_view> lines;
+    size_t start = 0;
+    while (true) {
+        size_t pos = buffer.find('\n', start);
+        if (pos == std::string::npos) break;
+        lines.push_back(std::string_view(buffer.data() + start, pos - start));
+        start = pos + 1;
     }
+    
+    int positions[] = {7, 6, 4, 3, 2, 0};
+    const int numPositions = sizeof(positions) / sizeof(positions[0]);
+    
+    std::vector<std::string_view> output;
+    for (int i = 0; i < numPositions; i++) {
+        countingSortByChar(lines, output, positions[i]);
+        lines.swap(output);
+    }
+    
+    for (const auto& sv : lines) {
+        std::cout << sv << "\n";
+    }
+    
+    return 0;
 }
